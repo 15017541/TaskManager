d26796b6954e70ee7acea3c5a227ee2405c5e7db
15017541 <15017541@myrp.edu.sg>
2017-05-30 12:46:23 +0800

    activity_add.xml

---
+++ app/src/main/java/android/myapplicationdev/com/taskmanager/AddActivity.java
@@ -0,0 +1,39 @@
+package android.myapplicationdev.com.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText etName, etDescription;
+    Button btnAddTask, btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        etName = (EditText) findViewById(R.id. etName);
+        etDescription = (EditText) findViewById(R.id. etDescription);
+        btnAddTask = (Button) findViewById(R.id. btnAddTask);
+        btnCancel = (Button) findViewById(R.id. btnCancel);
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/android/myapplicationdev/com/taskmanager/MainActivity.java
+++ app/src/main/java/android/myapplicationdev/com/taskmanager/MainActivity.java
@@ -1,7 +1,9 @@
 package android.myapplicationdev.com.taskmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -9,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnAddTask;
     ListView lv;
+    int requestCode = 12345;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
         btnAddTask = (Button) findViewById(R.id. btnAddTask);
         lv = (ListView) findViewById(R.id. lv);
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddActivity.class);
+                startActivityForResult(i, requestCode);
+            }
+        });
 
     }
 }
